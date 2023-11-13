4906ae1ae8a4baa44f2a12a77dcd9d9a0f80357d
brauliogm <brauliogm16@gmail.com>
2023-11-12 23:24:12 -0600

    Mostrando los datos del back en el front de angular

--- Front/inventario-app/src/app/app.module.ts
+++ Front/inventario-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { ProductoListaComponent } from './producto-lista/producto-lista.componen
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- Front/inventario-app/src/app/producto-lista/producto-lista.component.ts
+++ Front/inventario-app/src/app/producto-lista/producto-lista.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html'
 })
 export class ProductoListaComponent {
+  productos: Producto[];
 
+  constructor(private productoService: ProductoService){}
+
+  ngOnInit(){
+    //Cargar los productos
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos(){
+    //Consumir los datos del observable (suscribirnos)
+    this.productoService.obtenerProductosLista().subscribe(
+      (datos => {
+        this.productos = datos;
+      })
+    );
+  }
 }
